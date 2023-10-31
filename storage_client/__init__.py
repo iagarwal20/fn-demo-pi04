14ef18f69778eadd31e353f14e51a39aacea96f7
Ishitha Agarwal <iagarwal@akamai.com>
2023-10-31 16:47:20 +0530

    Creating function app

---
+++ storage_client/__init__.py
@@ -0,0 +1,32 @@
+import os, uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import logging
+import time
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    
+    try:
+        print("Azure Blob Storage Python quickstart sample")
+        account_url = os.getenv("ACCOUNT_URL")
+        cred = os.getenv("CLIENT_ID")
+        credential=DefaultAzureCredential(managed_identity_client_id = cred) 
+
+        # Create the BlobServiceClient object
+        while(True):
+            blob_service_client = BlobServiceClient(account_url, credential=credential)
+            container_name = str(uuid.uuid4())
+            container_client = blob_service_client.create_container(container_name)
+            print("Storage container created")
+            time.sleep(10)
+            blob_service_client.delete_container(container_name)
+            print("Storage container deleted")
+            blob_service_client.close()
+    except Exception as e:
+        logging.exception("Unable to connect to storage client"+ e)
+
+    return func.HttpResponse(
+             "Successfully updated inventory",
+             status_code=200
+        )
