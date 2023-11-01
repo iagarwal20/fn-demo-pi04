2aa4058b9114043e1cb5b87cdf4f8c1040967873
Ishitha Agarwal <iagarwal@akamai.com>
2023-11-01 11:55:33 +0530

    Fixed logging

--- storage_client/__init__.py
+++ storage_client/__init__.py
@@ -9,21 +9,19 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
     try:
-        print("Azure Blob Storage Python quickstart sample")
+        logging.info("Azure Blob Storage Python quickstart sample")
         account_url = os.getenv("ACCOUNT_URL")
         cred = os.getenv("CLIENT_ID")
         credential=DefaultAzureCredential(managed_identity_client_id = cred) 
 
-        # Create the BlobServiceClient object
-        while(True):
-            blob_service_client = BlobServiceClient(account_url, credential=credential)
-            container_name = str(uuid.uuid4())
-            container_client = blob_service_client.create_container(container_name)
-            print("Storage container created")
-            time.sleep(10)
-            blob_service_client.delete_container(container_name)
-            print("Storage container deleted")
-            blob_service_client.close()
+        blob_service_client = BlobServiceClient(account_url, credential=credential)
+        container_name = str(uuid.uuid4())
+        container_client = blob_service_client.create_container(container_name)
+        logging.info("Storage container created")
+        time.sleep(10)
+        blob_service_client.delete_container(container_name)
+        logging.info("Storage container deleted")
+        blob_service_client.close()
     except Exception as e:
         logging.exception("Unable to connect to storage client")
         logging.exception(e)
